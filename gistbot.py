6c52d24146127ca413254b23637c5c662e34570f
Lucas Farah <lucas.farah@me.com>
2018-01-28 15:48:51 -0200

    Almost finished with new HTML

--- gistbot.py
+++ gistbot.py
@@ -11,7 +11,7 @@ import os
 reload(sys)
 sys.setdefaultencoding('utf8')
 
-ghGist = Simplegist(username= os.environ['GITHUB_USER'], api_token= os.environ['GITHUB_TOKEN'])
+ghGist = Simplegist(username= 'lfarah', api_token= 'aab40008a8ef08521636c172487c0b995c67b0df')
 
 def getGist(index):
     lastGist = ghGist.profile().listall()[index]
@@ -22,16 +22,32 @@ def getGist(index):
         return lastGist
 
 lastGist = getGist(0)
-readmeMD = ghGist.search('lfarah').content(name=lastGist).split("#### Announcements")[1]
+mainContent = ghGist.search('lfarah').content(name=lastGist)
+announcements = mainContent.split("#### Announcements")[1].split("Links of the week")[0]
+links = mainContent.split("Links of the week")[1].split("Wanna see your library here")[0]
+libraries = mainContent.split("Here are the **5 awesome libraries of the week**:")[1]
 markdowner = Markdown()
-main = markdowner.convert(readmeMD)
+announcementsHTML = markdowner.convert(announcements)
+linksHTML = markdowner.convert(links)
+librariesHTML = markdowner.convert(libraries)
 libNumber = re.findall(r'\d+', lastGist)[0]
 header = "<h1 id=\"awesomeiosweekly" + libNumber  + "\">AwesomeiOS.Weekly [" + libNumber + "]</h1><p> </p><p> </p><p> </p><h4 id=\"announcements\">Announcements</h4>"
 footer = "That's all for this week! Please <a href=\"mailto:lucas.farah@me.com?Subject=Hello%20again\" target=\"_top\">give us feedback</a> and <a href=\"https://twitter.com/intent/tweet?text=AwesomeiOS%20weekly%20" + libNumber + "%20is%20out!%20http://bit.ly/2lJvy0P\" target=\"_top\">share on Twitter</a></h5></html>"
 
-with open('newsletterCSS.txt','r') as cssFile:
-    css = cssFile.read() + "<html>"
-
-    with open('output.txt','w') as output:
-        output.write(css + header + main + footer)
-        output.close()
+with open('header.html','r') as headerFile:
+    header = headerFile.read() + announcementsHTML
+    with open('links.html','r') as linksFile:
+        links = linksFile.read() + linksHTML
+        header += links
+        with open('libraries.html','r') as librariesFile:
+            libraries = librariesFile.read() + librariesHTML
+            header += libraries
+            with open('footer.html','r') as footerFile:
+                footer = footerFile.read()
+                header += footer
+                with open('output.html','w') as output:
+                    output.write(header)
+                    output.close()
+                    headerFile.close()
+                    linksFile.close()
+                    librariesFile.close()
